4deb8a6ed1177b6f36853a6781f912136f4720a8
Rainer Schreiber <rainer@evalentis.com>
2022-07-29 21:29:04 +0200

    Completed design of mat-sliders

--- src/app/mono-stereo/mono-stereo.component.ts
+++ src/app/mono-stereo/mono-stereo.component.ts
@@ -1,16 +1,38 @@
 import {Component, OnInit} from '@angular/core';
 
+export enum MonoStereoToggle {
+  mono, stereo
+}
+
 @Component({
   selector: 'app-mono-stereo',
   templateUrl: './mono-stereo.component.html',
   styleUrls: ['./mono-stereo.component.sass']
 })
+
 export class MonoStereoComponent implements OnInit {
 
+  get monoStereoToggleEnum(): typeof MonoStereoToggle {
+    return MonoStereoToggle
+  }
+
+  monoStereoToggle: MonoStereoToggle
+
   constructor() {
+
+    // TODO: fetch the real value from the tuner
+    this.monoStereoToggle = MonoStereoToggle.mono
+
   }
 
   ngOnInit(): void {
+  }
+
+
+  toggleMonoStereo() {
+    this.monoStereoToggle = this.monoStereoToggle == MonoStereoToggle.stereo ? MonoStereoToggle.mono : MonoStereoToggle.stereo
+
+    // TODO: send new value to the tuner
   }
 
 }
