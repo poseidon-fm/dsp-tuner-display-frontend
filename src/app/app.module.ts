7e9565bcaea55923876953835a5ed4912a26f3b8
Rainer Schreiber <rainer@evalentis.com>
2022-09-27 15:26:43 +0200

    First setup of Apollo GraphQL client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import {NgModule} from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
 
 import {MatSliderModule} from '@angular/material/slider';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
 import {SquelchComponent} from './squelch/squelch.component';
 import {MonoStereoComponent} from './mono-stereo/mono-stereo.component';
 import {FmsiComponent} from './fmsi/fmsi.component';
@@ -21,6 +28,8 @@ import {FmsiComponent} from './fmsi/fmsi.component';
     FmsiComponent
   ],
   imports: [
+    HttpClientModule,
+    ApolloModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
@@ -28,7 +37,20 @@ import {FmsiComponent} from './fmsi/fmsi.component';
     MatSliderModule,
     MatSlideToggleModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:8081/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
