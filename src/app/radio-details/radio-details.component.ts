f2984753866c93efdc5283c5a442138826c8dddd
Rainer Schreiber <rainer@evalentis.com>
2022-09-28 14:02:51 +0200

    Ping with pongVersion argument

--- src/app/radio-details/radio-details.component.ts
+++ src/app/radio-details/radio-details.component.ts
@@ -1,43 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
 import {Apollo, gql} from 'apollo-angular';
+
+const GET_PING_PONG = gql`
+  query PingPong($pongVersion: Int!) {
+    ping(pongVersion: $pongVersion) {
+      pong
+    }
+  }
+`;
 
 @Component({
   selector: 'radio-details-component',
   template: `
-    <div *ngIf="loading">
-      Loading...
-    </div>
-    <div *ngIf="error">
-      Error :(
-    </div>
     <div *ngIf="pong">
       <p>Ping: {{ pong }}</p>
     </div>
   `,
 })
-export class RadioDetailsComponent implements OnInit {
-  pong: string | undefined;
-  loading = true;
-  error: any;
+
+export class RadioDetailsComponent implements OnInit, OnDestroy {
+  pong: any;
+
+  private querySubscription: Subscription | undefined;
 
   constructor(private apollo: Apollo) {
   }
 
   ngOnInit() {
-    this.apollo
-    .watchQuery({
-      query: gql`
-        {
-          ping {
-            pong
-          }
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: GET_PING_PONG,
+        variables: {
+          pongVersion: 1
         }
-      `,
-    })
-    .valueChanges.subscribe((result: any) => {
-      this.pong = result?.data?.ping?.pong;
-      this.loading = result.loading;
-      this.error = result.error;
-    });
+      })
+      .valueChanges
+      .subscribe(({data}) => {
+        this.pong = data.ping.pong;
+      });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription?.unsubscribe();
   }
 }
